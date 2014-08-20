18151b18e3d0275799bb9b0ca6dd4f4e6834c949
Chetan <chetan@ATMECSINDT-049.(none)>
2014-08-20 15:58:17 +0530

    initial commit comment

---
+++ src/main/java/com/atmecs/com/atmecs/ReportCreator/App.java
@@ -0,0 +1,13 @@
+package com.atmecs.com.atmecs.ReportCreator;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/atmecs/com/atmecs/ReportCreator/TestClass.java
@@ -0,0 +1,59 @@
+/**
+ * @author chetan
+ *
+ */
+package com.atmecs.com.atmecs.ReportCreator;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TestClass {
+
+	@Test
+	public void test1() {
+		System.out.println("Inside test one");
+	}
+	
+	@Test
+	public void test2()
+	{
+		System.out.println("Inside test two");
+	}
+	
+	@Test
+	public void test3()
+	{
+		System.out.println("Inside test three");
+		//Assert.assertEquals("1", "0");
+	}
+	
+	@Test
+	public void test4()
+	{
+		System.out.println("Inside test four");
+	}
+	@Test
+	public void test5() {
+		System.out.println("Inside test five");
+	}
+	
+	@Test
+	public void test6()
+	{
+		System.out.println("Inside test six");
+		//Assert.assertEquals("1", "0");
+	}
+	
+	@Test
+	public void test7()
+	{
+		System.out.println("Inside test seven");
+	//	Assert.assertEquals("1", "0");
+	}
+	
+	@Test
+	public void test8()
+	{
+		System.out.println("Inside test eight");
+	}
+}
